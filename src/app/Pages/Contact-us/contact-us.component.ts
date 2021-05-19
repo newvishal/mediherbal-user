5d54c4de028e602ab108bbdb62125b31b409f182
Khushboo Thakur <khushboothakur2305@gmail.com>
2021-05-19 19:05:04 +0530

    feat(contact): add contact ui

--- src/app/Pages/Contact-us/contact-us.component.ts
+++ src/app/Pages/Contact-us/contact-us.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.scss']
+  styleUrls: ['./contact-us.component.scss'],
 })
 export class ContactUsComponent implements OnInit {
-
-  constructor() { }
-
+  /*
+  @description:From Variable
+  */
+  contactUsForm: FormGroup;
+  constructor() {}
+  /*
+  initializing the contactUsForm by form group
+  */
   ngOnInit(): void {
+    this.contactUsForm = new FormGroup({
+      full_name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required]),
+      message: new FormControl('', [Validators.required]),
+    });
   }
+  /*
+  @description:this method is executed in submit of the signup form
+  */
 
+  onSubmit() {
+    console.log(this.contactUsForm.value);
+  }
 }
