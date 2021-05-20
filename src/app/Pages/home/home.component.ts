6218d5c9e7aae9e82224afbce1ce70e56e70ecaa
Ayush Mishra <ayushmishra0810@gmail.com>
2021-05-21 05:22:01 +0530

    Updatyes

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -10,8 +10,10 @@ export class HomeComponent implements OnInit {
   constructor(private homeService: HomeService) {}
 
   ngOnInit(): void {
+    /* this method dispatch Actions From Service */
     this.homeService.fetchComboProducts();
     this.homeService.fetchProducts();
+    /* this method return data observable Service */
     this.homeService.getProducts().subscribe((products) => {
       console.log(products);
     });
