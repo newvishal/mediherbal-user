7381bb2c4d523db2aae446c18d04f79afaff8abd
khushboothakur2305 <khushboothakur2305@gmail.com>
2021-05-29 13:50:48 +0530

    feat(cart) : add cart UI

--- src/app/Pages/cart/cart.module.ts
+++ src/app/Pages/cart/cart.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from 'src/app/shared/Module/material/material.module';
 import { SharedModule } from 'src/app/shared/Module/shared/shared.module';
 import { CartRoutingModule } from './cart-routing.module';
+import { CartComponent } from './cart.component';
 
 @NgModule({
   imports: [SharedModule, MaterialModule, CartRoutingModule],
-  declarations: [],
+  declarations: [CartComponent],
 })
 export class CartModule {}
