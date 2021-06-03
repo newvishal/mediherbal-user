e343f466967211e5481c90ad339500e883b4f71f
Ayush Mishra <ayushmishra0810@gmail.com>
2021-06-03 13:48:03 +0530

    Address Added

---
+++ src/app/Pages/user-address/components/address-card/address-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-address-card',
+  templateUrl: './address-card.component.html',
+  styleUrls: ['./address-card.component.scss']
+})
+export class AddressCardComponent implements OnInit {
+@Input()useraddress;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/Pages/user-address/user-address.module.ts
+++ src/app/Pages/user-address/user-address.module.ts
@@ -4,9 +4,10 @@ import { SharedModule } from 'src/app/shared/Module/shared/shared.module';
 import { UserAddressRoutingModule } from './user-address-routing.module';
 import { UserAddressComponent } from './user-address.component';
 import { AddAddressComponent } from './components/add-address/add-address.component';
+import { AddressCardComponent } from './components/address-card/address-card.component';
 
 @NgModule({
   imports: [SharedModule, MaterialModule, UserAddressRoutingModule],
-  declarations: [UserAddressComponent, AddAddressComponent],
+  declarations: [UserAddressComponent, AddAddressComponent, AddressCardComponent],
 })
 export class UserAddressModule {}
