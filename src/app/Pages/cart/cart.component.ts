5183f783dc9c478077b30c64f0c159b213df31f8
khushboothakur2305 <khushboothakur2305@gmail.com>
2021-06-04 19:47:44 +0530

    feat(cart): add data to DOM

--- src/app/Pages/cart/cart.component.ts
+++ src/app/Pages/cart/cart.component.ts
@@ -12,11 +12,47 @@ import * as fromCartAction from './store/cart.actions';
 })
 export class CartComponent implements OnInit {
   constructor(private cartSerice: CartService) {}
-
+  cartData;
+  AmountDetails = {
+    Total: 0,
+    TotalMRP: 0,
+    DiscountMrp: 0,
+  };
   ngOnInit(): void {
     this.cartSerice.fetchCart();
     this.cartSerice.CartDeatilsSubject.subscribe((cartDetails) => {
+      this.cartData = cartDetails;
       console.log(cartDetails);
+      if (cartDetails.length > 0) {
+        console.log('Data');
+
+        cartDetails.map((cart) => {
+          console.log(cart);
+
+          if (cart.cartData.product_type === 'products') {
+            console.log('product');
+            cart.product_data.product_type.map((productType) => {
+              if (
+                productType.product_id === cart.cartData.selected_product_id
+              ) {
+                this.AmountDetails.Total =
+                  this.AmountDetails.Total +
+                  productType.price * cart.cartData.quantity;
+                console.log(productType.price * cart.cartData.quantity);
+              }
+            });
+          }
+          if (cart.cartData.product_type === 'comboProduct') {
+            console.log('combo');
+
+            this.AmountDetails.Total =
+              this.AmountDetails.Total +
+              cart.product_data.price * cart.cartData.quantity;
+            console.log(cart.product_data.price * cart.cartData.quantity);
+          }
+        });
+      }
+      console.log(this.AmountDetails);
     });
   }
 }
--- src/app/Pages/cart/service/cart.service.ts
+++ src/app/Pages/cart/service/cart.service.ts
@@ -11,7 +11,7 @@ export class CartService {
     private angularFireStore: AngularFirestore,
     private store: Store<fromApp.AppState>
   ) {}
-  CartDeatilsSubject = new BehaviorSubject([]);
+  CartDeatilsSubject = new BehaviorSubject(null);
   fetchCart() {
     this.store
       .select('AuthSection')
@@ -19,16 +19,37 @@ export class CartService {
         pluck('user'),
         pluck('cart'),
         map((userCart) => {
-          console.log(userCart);
-
           let updatedCartDetails: any[] = [];
           if (userCart) {
             userCart.map((cartitem) => {
-              console.log(cartitem);
-
               if (cartitem.product_type === 'products') {
                 this.angularFireStore
                   .collection('products')
+                  .doc(cartitem.product_id)
+                  .snapshotChanges()
+                  .pipe(
+                    take(1),
+                    map((product) => {
+                      let returnData;
+                      const data = product.payload.data() as any;
+                      const id = product.payload.id;
+                      return (returnData = {
+                        product_data: { ...data },
+                        cartData: { ...cartitem },
+                        product_id: id,
+                      });
+                    }),
+                    map((cartDetails) => {
+                      updatedCartDetails.push({
+                        ...cartDetails,
+                      });
+                    })
+                  )
+                  .subscribe();
+              }
+              if (cartitem.product_type === 'comboProduct') {
+                this.angularFireStore
+                  .collection('combo-products')
                   .doc(cartitem.product_id)
                   .snapshotChanges()
                   .pipe(
@@ -56,7 +77,8 @@ export class CartService {
           return updatedCartDetails;
         }),
         map((cartDeatils) => {
-          this.CartDeatilsSubject.next(cartDeatils);
+          const finalData = cartDeatils;
+          this.CartDeatilsSubject.next(finalData);
         })
       )
       .subscribe();
