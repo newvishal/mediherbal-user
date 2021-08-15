2a2b4c28652cc1c8204dde8a42a8d59b240d565f
Ayush Mishra <ayushmishra0810@gmail.com>
2021-08-15 18:29:58 +0530

    Chnages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  base_url: 'https://manish-mediherbal-1981.herokuapp.com/',
+  firebase: {
+    apiKey: 'AIzaSyDiXio31XreiLzQOGItHAHIh6h1R2JI0RI',
+    authDomain: 'mediherbal-e7901.firebaseapp.com',
+    projectId: 'mediherbal-e7901',
+    storageBucket: 'mediherbal-e7901.appspot.com',
+    messagingSenderId: '129354040723',
+    appId: '1:129354040723:web:2b505947b0488aebd8d466',
+    measurementId: 'G-NW4MVGMJDF',
+  },
 };
